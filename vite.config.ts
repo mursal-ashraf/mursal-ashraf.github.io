32b8c94d932c257dec734c5d9f7aa87f9c299ddc
Mursal Ashraf <mash0019@student.monash.edu>
2025-06-04 22:53:26 +1000

    update vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: 'https://mursal-ashraf.github.io'
 })

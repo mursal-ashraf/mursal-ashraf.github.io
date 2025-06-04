51931ec7e90f378ed034a4a9b9b7ff1290c9d09e
Mursal Ashraf <mash0019@student.monash.edu>
2025-06-04 22:58:39 +1000

    rename workflow file

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://mursal-ashraf.github.io'
+  plugins: [react()]
 })

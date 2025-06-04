3ba11b4590a00c1b1fe324d19f9916b880fa089e
Mursal Ashraf <mash0019@student.monash.edu>
2025-06-04 22:33:42 +1000

    Fix (attempt): remove base from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/mursal-ashraf.github.io/'
+  plugins: [react()]
 })
